ab61a2ec555dcad83d293a6ddcee5a0ae0e0c2d1
R. Anthony Porter <rah@sheenonline.biz>
2017-03-18 03:04:07 -0400

    Added redux-form dep and entered form layout.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { reduxform  } from 'redux-form';
 
 class PostsNew extends Component {
     render() {
         return(
-            <div>Create Form</div>
+            <form>
+                <h3>Create A New Post</h3>
+                <div className="form-group">
+                    <label htmlFor="">Title</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Categories</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="">Content</label>
+                    <textarea className="form-control" />
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         )
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+})(PostsNew);
